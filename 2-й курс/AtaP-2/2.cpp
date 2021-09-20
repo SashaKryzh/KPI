a004f2c21179ff92c9e80e0a90a5d2a293bc66af
Alexander <sashakryzh@gmail.com>
2021-09-20 11:41:03 +0300

    AtaP KP-2

--- 2-й курс/AtaP-2/2.cpp
+++ 2-й курс/AtaP-2/2.cpp
@@ -5,18 +5,51 @@ namespace MedicineType
 {
 	enum type
 	{
-		HOSPITAL,
+		HOSPITAL = 0,
 		CLINIC,
 		AMBULATORY,
 		PHARMACY,
 		LABORATORY,
 		URGENTCARE,
+		ERROR,
 	};
 }
 
 int main(void)
 {
 	MedicineType::type type;
-	scanf("%d", &type);
+	
+	unsigned short shType;
+	scanf("%hu", &shType);
+	type = shType < MedicineType::ERROR ? (MedicineType::type)shType : MedicineType::ERROR;
+	
+	const char *s;
+	switch (type)
+	{
+	default:
+		s = "ERROR";
+		break;
+	case MedicineType::HOSPITAL:
+		s = "HOSPITAL";
+		break;
+	case MedicineType::CLINIC:
+		s = "CLINIC";
+		break;
+	case MedicineType::AMBULATORY:
+		s = "AMBULATORY";
+		break;
+	case MedicineType::PHARMACY:
+		s = "PHARMACY";
+		break;
+	case MedicineType::LABORATORY:
+		s = "LABORATORY";
+		break;
+	case MedicineType::URGENTCARE:
+		s = "URGENTCARE";
+		break;
+	}
+
+	printf("%s\n", s);
+
 	return 0;
 }
